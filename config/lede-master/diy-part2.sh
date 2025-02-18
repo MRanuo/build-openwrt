bf5dca4b6d3434499daf17407f1eea3cc3c9590e
MRanuo <69956918+MRanuo@users.noreply.github.com>
2025-02-18 21:40:09 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -30,7 +30,7 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 #
 # Add luci-app-amlogic
 #svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-svn co https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-app-istorex
+svn co https://github.com/kenzok8/openwrt-packages luci-app-istorex
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
